b8eef20ba1502c52861d35cc9650c46db72af97e
Gabriel Gonçalves Blankenburg <gabriel.blankenburg.p.silva@gmail.com>
2018-07-21 16:46:55 -0300

    Adicionado o teste de create

--- src/tests/Unit/UserApiTest.php
+++ src/tests/Unit/UserApiTest.php
@@ -17,23 +17,21 @@ class UserApiTest extends TestCase
     public function create()
     {
 
-    	// $user = [
-    	// 	'name' => 'José',
-    	// 	'rg' => '1312312',
-    	// 	'cpf' => '1312312',
-    	// 	'email' => 'jose@email.com',
-    	// 	'birth_date' => '1998-10-10',
-    	// 	'password' => '123456',
-    	// ];
+    	$user = [
+    		'name' => 'José',
+    		'rg' => '1312312',
+    		'cpf' => '1312312',
+    		'email' => 'jose@email.com',
+    		'birth_date' => '1998-10-10',
+    		'password' => '123456',
+    	];
 
-    	// $response = $this->withHeaders([
-     //    	'Content-Type' => 'application/x-www-form-urlencoded',
-     //    	'X-Requested-With' => 'XMLHttpRequest'
-     //    ])->json('POST', '/api/users', $user);
+    	$response = $this->withHeaders([
+        	'Content-Type' => 'application/x-www-form-urlencoded',
+        	'X-Requested-With' => 'XMLHttpRequest'
+        ])->json('POST', '/api/users', $user);
         
-     //    $response
-     //        ->assertStatus(201);
-
-    	$this->assertTrue(false);
+        $response
+            ->assertStatus(201);
     }
 }
